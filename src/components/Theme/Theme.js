4f39fbcea7bba0ff046be71aaaa96a2ed9f7cb4f
Devon A Loy <dloy3000@gmail.com>
2023-01-19 18:58:12 -0500

    Setup theme.

--- src/components/Theme/Theme.js
+++ src/components/Theme/Theme.js
@@ -2,12 +2,22 @@ import {
     extendTheme,
     useColorModeValue
 } from "@chakra-ui/react"
+import palette from "../../data/theme/palette.json";
+import font from "../../data/theme/font.json";
 
-const theme = extendTheme({
-    fonts: {
-        heading: `'Asgalt', sans-serif`,
-        body: `'Modeka', sans-serif`,
-    },
-})
+//Updated version default breakpoints
+const breakpoints = {
+    sm: '30em',
+    md: '48em',
+    lg: '62em',
+    xl: '80em',
+    '2xl': '96em',
+    '3xl': '124em',
+    '4xl': '148em',
+    '5xl': '240em',
+    '6xl': '320em',
+}
+
+const theme = extendTheme(palette, font, { breakpoints });
 
 export default theme;
